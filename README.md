e91f9f69e07dbf5b4ab04bd03445f3ed3f2f9884
DaniloLMD <danilolmd04@gmail.com>
2023-04-11 23:25:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# C-Pietro-Martins
+ Curso de C no YT por Pietro Martins
---
+++ basicos/scanf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int idade;
+	printf("Escreva sua idade: ");
+	scanf("%d", &idade);
+	printf("Voce foi registrado com %d anos.", idade);
+	
+}
